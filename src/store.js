4e1c018078602d5ca9efba6feaab5b49de907644
camilocls <camiloclsx@gmail.com>
2018-10-11 07:57:15 -0500

    Fetch data products and categories

--- src/store.js
+++ src/store.js
@@ -4,7 +4,48 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    categories: [],
+    products: [],
+    cart: []
+  },
+  mutations: {
+    SET_PRODUCTS(state, products) {
+      state.products = products;
+    },
+    SET_CATEGORIES(state, categories) {
+      state.categories = categories;
+    },
+    SET_CAR(state, products) {
+      state.cart = products;
+    }
+  },
+  actions: {
+    setProducts({ commit }) {
+      fetch("./data/products.json")
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Oops! This data can not be found.");
+          }
+        })
+        .then(data => {
+          commit("SET_PRODUCTS", data.products);
+        });
+    },
+    setCategories({ commit }) {
+      fetch("./data/categories.json")
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Oops! This data can not be found.");
+          }
+        })
+        .then(data => {
+          commit("SET_CATEGORIES", data.categories);
+        });
+    }
+  }
 });
