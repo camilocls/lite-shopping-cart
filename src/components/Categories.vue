278444c5a922848650523a653b67fe9c0a2d4dd5
camilocls <camiloclsx@gmail.com>
2018-10-11 18:09:02 -0500

    - Add component to list Categories

---
+++ src/components/Categories.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="categories">
+    <TreeCategorie
+      v-for="categorie in categories"
+      :key="categorie.id"
+      :id="categorie.id"
+      :levels="categorie.sublevels"
+      :label="categorie.name"
+    />
+  </div>
+</template>
+
+<script>
+import TreeCategorie from "./TreeCategorie.vue";
+
+export default {
+  name: "Categories",
+  props: {
+    categories: Array
+  },
+  components: {
+    TreeCategorie
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.categories {
+  margin: 0 0 30px;
+}
+</style>
