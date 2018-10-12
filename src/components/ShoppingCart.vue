bddca4a699bc00f7ba9da3ed84dddbf85349ea87
camilocls <camilocls@users.noreply.github.com>
2018-10-12 02:42:37 -0500

    - Add total price - Improve computed values - Add button Buy

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -3,16 +3,26 @@
     <h2 class="shopping-cart__title">Shopping Cart</h2>
     <div class="items">
       <ProductCart
-        v-for="product in getProductsInCart"
+        v-for="product in productsInCart"
         :key="product.id"
         :product="product"
       />
+    </div>
+    <div class="shopping-cart__total">
+      Total: <strong>${{totalPriceCart|setCurrency}}</strong>
+    </div>
+    <div v-if="!checkout&&productsInCart.length" class="shopping-cart__checkout">
+      <button @click="setheckout()" class="shopping-cart__buy">Buy</button>
+    </div>
+    <div v-if="checkout" class="shopping-cart__checkout-success">
+      Your order has been sent.
     </div>
   </div>
 </template>
 
 <script>
 import ProductCart from "./ProductCart.vue";
+import { mapState, mapGetters } from "vuex";
 
 export default {
   name: "ShoppingCart",
@@ -20,8 +30,12 @@ export default {
     ProductCart
   },
   computed: {
-    getProductsInCart() {
-      return this.$store.getters.productsInCart;
+    ...mapState(["checkout"]),
+    ...mapGetters(["productsInCart", "totalPriceCart"])
+  },
+  methods: {
+    setheckout() {
+      return this.$store.dispatch("setCheckout", true);
     }
   }
 };
@@ -35,5 +49,28 @@ export default {
 }
 .items__item {
   display: flex;
+}
+.shopping-cart__total {
+  font-size: 24px;
+  margin-bottom: 30px;
+}
+.shopping-cart__buy {
+  width: 100%;
+  display: block;
+  background-color: #2dc560;
+  border: 0;
+  border-radius: 4px;
+  color: #fff;
+  padding: 10px 20px;
+  font-weight: 700;
+  font-size: 20px;
+  cursor: pointer;
+  transition: background-color ease 0.3s;
+}
+.shopping-cart__buy:hover {
+  background-color: #06a73c;
+}
+.shopping-cart__checkout-success {
+  font-size: 24px;
 }
 </style>
