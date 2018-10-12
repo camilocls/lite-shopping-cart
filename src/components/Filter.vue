df1eab73d95ede3a520afb18d43c2f7c14c2a034
camilocls <camilocls@users.noreply.github.com>
2018-10-12 02:45:39 -0500

    Add Filter component

---
+++ src/components/Filter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="filter">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Filter",
+  props: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
