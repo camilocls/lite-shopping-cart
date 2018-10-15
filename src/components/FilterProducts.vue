255478eb8d6a3f26cd9eac8307fc4594805e70af
camilocls <camilocls@users.noreply.github.com>
2018-10-15 17:36:02 -0500

    Add filters

--- src/components/FilterProducts.vue
+++ src/components/FilterProducts.vue
@@ -1,16 +1,56 @@
 <template>
   <div class="filter">
-    WIP: Filter Here
+    <h3>Order by:</h3>
+    <select v-model="filter" @change="filterProducts" class="field__select" id="field-price">
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option.value">
+      {{option.label}}
+      </option>
+    </select>
   </div>
 </template>
 
 <script>
 export default {
   name: "FilterProducts",
-  props: {}
+  data() {
+    return {
+      filter: "",
+      options: [
+        { label: "Select a option", value: "" },
+        { label: "Price: Low to High", value: "priceLowHigh" },
+        { label: "Price: High to Low", value: "priceHighLow" },
+        { label: "Quantity", value: "quantity" },
+        { label: "Available", value: "available" }
+      ]
+    };
+  },
+  props: {},
+  methods: {
+    filterProducts() {
+      this.$store.commit({
+        type: "SET_FILTER",
+        filter: this.filter
+      });
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.filter {
+  margin-bottom: 18px;
+  display: flex;
+  align-items: center;
+}
+.field__select {
+  border: 0;
+  background-color: #eee;
+  border-radius: 4px;
+  padding: 8px 12px;
+  margin-left: 20px;
+}
 </style>
